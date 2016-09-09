e91dd51be3501aa678eba377e7f89e2d730aee83
debian17 <forletters01@gmail.com>
2016-09-09 13:03:46 +0300

    C#
    Laba1

---
+++ Labs/C#/Laba1/src/Laba1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace Laba1
+{
+    public class DebugPrintAttribute: Attribute
+    {
+        public string Format { get; set; }
+
+        public DebugPrintAttribute(string Format="{0}")
+        {
+            this.Format = Format;
+        }
+    }
+    
+    [DebugPrint]
+    public static class RefLab
+    {
+        public static void DebugPrint(object obj)
+        {
+            
+
+        }
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Type t = typeof(RefLab);
+            object[] attrs = t.
+            foreach (DebugPrintAttribute a in attrs)
+            {
+                Console.WriteLine(a.Format);
+            }
+
+        }
+    }
+}
---
+++ Labs/C#/Laba1/src/Laba1/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Laba1")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("515381cc-f013-4c76-befa-a4ea5d3faf1b")]
