9cb45685b78f6c1eca84a5f2b90f4f2a427ce37e
debian17 <forletters01@gmail.com>
2016-10-19 22:51:38 +0300

    add lab2 c#

--- Labs/C#/Laba2/src/Laba2/Program.cs
+++ Labs/C#/Laba2/src/Laba2/Program.cs
@@ -7,34 +7,52 @@ namespace Laba2
 {
     public class Place
     {
+        public bool[] cars;
+        public int[] income;
+        public int[] outcome;
 
+        public Place()
+        {
+            cars = new bool[3];
+            income = new int[3];
+            outcome = new int[3];
+        }
     }
 
     public class Station
-    {
-        private const int n = 0;
-        private const int N = 10;
-        private const int k = 0;
-        private const int K = 10;
-
+    {    
         private int m;
         private Place[] M;
 
         public Station(int m)
         {
             this.m = m;
+            M = new Place[m];
         }
 
+        public void AddCar(int n, int N, int k, int K)
+        {
+            Random r = new Random();
+            for(int )
 
+        }
     }
-
 
     public class Program
     {
-        
+        const int n = 0;
+        const int N = 10;
+        const int k = 0;
+        const int K = 20;
 
         public static void Main(string[] args)
         {
+            Console.Write("Vvedite kolichestvo mest: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Station S = new Station(m);
+
+            
+
         }
     }
 }
