ef9dd2e3d94ae9f3d3f71814b26afd8bfb29cd61
debian17 <forletters01@gmail.com>
2016-10-24 14:47:38 +0300

    add web lab1

---
+++ Labs/C#/TestNet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+        }
+    }
+}
