a8ce3475ecab1f4d941e572f6fc6e9d9a9d2ba29
debian17 <forletters01@gmail.com>
2017-02-01 20:43:14 +0300

    add laba1 android

--- Android/Laba1/app/src/main/java/com/example/laba1/MainActivity.java
+++ Android/Laba1/app/src/main/java/com/example/laba1/MainActivity.java
@@ -1,46 +1,42 @@
 package com.example.laba1;
 
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import static com.example.laba1.R.string.Maintext;
-import static com.example.laba1.R.string.c1;
-import static com.example.laba1.R.string.c2;
-import static com.example.laba1.R.string.c3;
-import static com.example.laba1.R.string.tc1;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView text;
-    private TextView contact1;
-    private TextView contact2;
-    private TextView contact3;
-    private TextView ct1;
-    private TextView ct2;
-    private TextView ct3;
-    private ImageView im1;
+    private TextView textView;
+    private ArrayList<Item> items;
+    private RecyclerView RV;
+    private ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String p1 = "@drawable/i1";
-        text = (TextView) findViewById(R.id.List_contact);
-        text.setText(Maintext);
+        textView = (TextView)findViewById(R.id.Contact_list);
+        textView.setText(R.string.c_list);
+        items = new ArrayList<Item>();
 
-        contact1 = (TextView)findViewById(R.id.contact1);
-        contact1.setText(c1);
+        items.add(new Item("Печальная жаба","Я печальна жаба и жизнь моя печальна",
+                getResources().getIdentifier("@drawable/item1", null, getPackageName()),"#e6eeff"));
+        items.add(new Item("Ждун","Я тут подожду пока...",
+                getResources().getIdentifier("@drawable/item2", null, getPackageName()),"#ffffb3"));
+        items.add(new Item("Вжух","Вжух и ты сдал лабу!",
+                getResources().getIdentifier("@drawable/item3", null, getPackageName()),"#800000"));
 
-        ct1 = (TextView) findViewById(R.id.tc1);
-        ct1.setText(tc1);
-
-        im1 = (ImageView) findViewById(R.id.pic1);
-        int imageResource = getResources().getIdentifier(p1, null, getPackageName());
-        Drawable res = getResources().getDrawable(imageResource);
-        im1.setImageDrawable(res);
-
+        RV = (RecyclerView)findViewById(R.id.RV_Items);
+        itemAdapter = new ItemAdapter(this, items);
+        RV.setAdapter(itemAdapter);
+        RV.setLayoutManager(new LinearLayoutManager(this));
     }
 }
